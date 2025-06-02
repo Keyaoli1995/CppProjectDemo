2fd6cda1249a861d38645df5d59453eaf032019b
Keyao.Li <likeyao1995@163.com>
2025-06-02 19:59:08 +0800

    ##
    简化 main.cpp 中的命名空间和返回语句
    
    在 `main.cpp` 文件中，添加了 `using namespace std;` 以简化命名空间使用。同时，去掉了 `return 0;` 语句中的注释，使代码更加简洁。

--- 00_HelloWorld/src/main.cpp
+++ 00_HelloWorld/src/main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>  
 
+
 using namespace std;  
 
 int main(void) {  
     cout << "Hello, World!" << endl;    
-    return 0; // Add this line to return an integer value.  
+    return 0; 
 }
